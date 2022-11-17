985ac40c4310efb9a74aa5e06deafdc45236d319
Ana <estudioj.ferreira@gmail.com>
2022-11-16 23:52:32 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+
+import io from "socket.io-client"
+import { useEffect } from "react"
+
+
+
+//paso el url del socket del server
+//conexion con el socket server 
+//tenemos una conexion para emit o listen to events 
+const socket = io.connect("http://localhost:3001")
 
 function App() {
+
+  //utilizamos esa conexion para enviar/emit un mensaje
+  //va a ser recibido por el back, 
+  //y va a emitir otro mensaje para q sea recibido
+  //el back es necesario como layer para conectar los events
+
+const sendMessage = () =>{
+  //le damos nombre al evento q emitimos
+  //y podemos mandar data 
+  //el back tiene q escuchar el evento con este nombre para recibirlo
+socket.emit("send-message", {message: "Hello"})
+}
+
+//esta funcion es llamada siempre q se recibe un mensaje
+//por eso [socket]
+useEffect(()=>{
+socket.on("receive_message", (data) =>{
+  alert(data.message)
+})
+
+},[socket])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <input placeholder="message"/>
+     <button onClick={sendMessage}>send message</button>
     </div>
   );
 }
